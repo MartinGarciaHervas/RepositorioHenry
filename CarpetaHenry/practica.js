badf2ea039470bd18d39591a0a91897d892e8eac
MartinGarciaHervas <punighervas@gmail.com>
2023-06-22 13:44:09 -0300

    Prueba

---
+++ CarpetaHenry/practica.js
@@ -0,0 +1,19 @@
+var jamon = ["pepe", "alma", "roberto", "angela", "lechuga", "andalaconcha"];
+
+
+function filter(arrayOfStrings) {
+   // Debes identificar todos los elementos del arreglo que comiencen con la letra "a".
+   // Luego retorna un nuevo arreglo con estos elementos.
+   // Tu código:
+   var newArray = [];
+
+   for(var i = 0; i < arrayOfStrings.length; i ++){
+      if((arrayOfStrings[i].split(""))[0] === "a"){
+         newArray.push(arrayOfStrings[i]);
+      };
+   }
+
+   return newArray;
+}
+
+console.log(filter(jamon));
